c5e06dbee6f6eaf20792d3c989724368850e41e1
Kamthorn Kwunsanaye <kkwunsanaye@hotmail.com>
2020-07-14 19:30:28 +0700

    EP04

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kamfood/utility/my_style.dart';
 
 class SignUp extends StatefulWidget {
   @override
@@ -12,6 +13,82 @@ class _SignUpState extends State<SignUp> {
       appBar: AppBar(
         title: Text('Sign Up'),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(30.0),
+        children: <Widget>[
+          myLogo(),
+          MyStyle().mySizebox(),
+          showAppName(),
+          MyStyle().mySizebox(), nameForm(),
+        ],
+      ),
     );
   }
+
+  Widget nameForm() => Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            Icons.account_box,
+            color: MyStyle().darkColor,
+          ),
+          labelStyle: TextStyle(color: MyStyle().darkColor),
+          labelText: 'Name :',
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: MyStyle().darkColor)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: MyStyle().priaryColor)),
+        ),
+      ));
+
+ Widget userForm() => Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            Icons.account_box,
+            color: MyStyle().darkColor,
+          ),
+          labelStyle: TextStyle(color: MyStyle().darkColor),
+          labelText: 'User :',
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: MyStyle().darkColor)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: MyStyle().priaryColor)),
+        ),
+      ));
+
+ Widget passwordForm() => Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            Icons.account_box,
+            color: MyStyle().darkColor,
+          ),
+          labelStyle: TextStyle(color: MyStyle().darkColor),
+          labelText: 'Password :',
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: MyStyle().darkColor)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: MyStyle().priaryColor)),
+        ),
+      ));
+
+  Row showAppName() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        MyStyle().showTitle('Kam Food'),
+      ],
+    );
+  }
+
+  Widget myLogo() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          MyStyle().showLogo(),
+        ],
+      );
 }
